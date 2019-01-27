4232931eb3eab1196546a58b9c3f91f7404be654
Anisse Astier <anisse@astier.eu>
2019-01-27 21:22:53 +0100

    initial spotify support
    We read playlist names in files, and then play them

--- beatbox.go
+++ beatbox.go
@@ -5,10 +5,13 @@ import (
 	"encoding/hex"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"os"
 	"path/filepath"
 	"strings"
 	"time"
+
+	"github.com/librespot-org/librespot-golang/src/librespot/core"
 )
 
 const (
@@ -20,54 +23,81 @@ const (
 	NEXTDELAY = 5 * time.Second
 )
 
-type playerState struct {
+type player struct {
 	tag     string
 	list    []string
 	index   int
 	stopped time.Time
+	spotify bool
+	session *core.Session
+	stop    context.CancelFunc
 }
 
-func (p *playerState) play(w chan<- error) context.CancelFunc {
+func (p *player) play(w chan<- error) context.CancelFunc {
 	ctx, stop := context.WithCancel(context.Background())
 	fmt.Println("Now playing:", p.list[p.index])
 	go func() {
-		w <- playMp3(ctx, p.list[p.index])
+		if p.spotify {
+			playTrack(ctx, p.session, p.list[p.index])
+		} else {
+			w <- playMp3(ctx, p.list[p.index])
+		}
 	}()
 	return stop
 }
+func newPlayer() (*player, error) {
+	session, err := openSession()
+	if err != nil {
+		return nil, err
+	}
+	return &player{
+		session: session,
+		stop:    func() {},
+	}, nil
+}
 
-func player(c <-chan string) {
-	var stop context.CancelFunc = func() {}
-	var p playerState
+func (p *player) player(c <-chan string) {
 	w := make(chan error)
 	for {
 		select {
 		case s := <-c:
 			switch {
 			case s == SHUTDOWN:
-				stop()
+				p.stop()
 			case strings.HasPrefix(s, STOPTAG):
 				p.stopped = time.Now()
-				stop()
+				p.stop()
 			case s == p.tag && time.Since(p.stopped) < NEXTDELAY &&
 				len(p.list) > p.index+1:
 				//next
 				p.index++
 
-				stop()
-				stop = p.play(w)
+				p.stop()
+				p.stop = p.play(w)
 			default:
 				list, err := filepath.Glob("/perm/beatbox-data/" + s + "/*.mp3")
 				if err != nil || len(list) == 0 {
-					fmt.Println("Unknown tag/command:", s)
-					continue
+					// try with spotify
+					if playlist, err := ioutil.ReadFile("/perm/beatbox-data/" + s); err == nil && len(playlist) > 0 {
+						list, err = playlistTracks(p.session, string(playlist))
+						if err != nil {
+							fmt.Println("Unknown playlist/track:", err.Error(), string(playlist), s)
+							continue
+						}
+						p.spotify = true
+					} else {
+						fmt.Println("Unknown tag/command:", s)
+						continue
+					}
+				} else {
+					p.spotify = false
 				}
 				p.index = 0 //first
 				p.tag = s
 				p.list = list
 
-				stop()
-				stop = p.play(w)
+				p.stop()
+				p.stop = p.play(w)
 			}
 		case err := <-w:
 			if err != nil {
@@ -79,7 +109,7 @@ func player(c <-chan string) {
 			//next
 			if len(p.list) > p.index+1 {
 				p.index++
-				stop = p.play(w)
+				p.stop = p.play(w)
 			}
 		}
 	}
@@ -88,11 +118,16 @@ func player(c <-chan string) {
 func main() {
 	f, err := os.Open("/dev/hidraw0")
 	if err != nil {
-		fmt.Println("Error", err)
+		fmt.Println("Error opening HID device", err)
+		return
+	}
+	p, err := newPlayer()
+	if err != nil {
+		fmt.Println("Error creating player", err)
 		return
 	}
 	c := make(chan string)
-	go player(c)
+	go p.player(c)
 
 	for {
 		t := make([]byte, TAGLEN)
